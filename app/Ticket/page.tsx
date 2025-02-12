faeac306bdfc01cfec5dab4cb558f27af98078aa
Muritala Ahmed <muritalaahmed405@gmail.com>
2025-02-12 08:33:39 +0100

    wrapped ticket component in suspense

--- app/Ticket/page.tsx
+++ app/Ticket/page.tsx
@@ -1,14 +1,18 @@
 "use client"
-import React from "react";
+import React, { Suspense } from "react";
 import Header from "../Header";
 import Ticket from "../Ticket";
+import Loading from "../Loading";
 
 export default function Page() {
   return (
     <div className="bg-background min-h-screen w-full px-5 py-5 md:px-[10rem]">
       <Header/>
+     <Suspense fallback={<div><Loading/></div>}>
      <Ticket/>
-    
+          
+        
+        </Suspense>
       {}
     </div>
   );
